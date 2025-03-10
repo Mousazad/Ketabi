7b2cca22351205238cdf7c4eebe1dde426b8261c
s-ras <rasolisepehr@gmail.com>
2025-03-10 11:28:26 +0330

    added a view for book information, added api route and form for creating books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -1,15 +1,35 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Book;
+use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function index()
-	{
-		$A = Book::all();
-		return view('books.index', ['books'=>$A]);
-	}
+    {
+        $A = Book::all();
+        return view('books.index', ['books' => $A]);
+    }
+
+    public function show($id)
+    {
+        $book = Book::find($id);
+        if (! $book) {
+            return abort(404);
+        }
+        return view('books.show', ['book' => $book]);
+    }
+
+    public function create(Request $request)
+    {
+        $validated = $request->validate([
+            'title'            => 'required|string|max:255',
+            'publication_year' => 'required|int|min:0',
+        ]);
+
+        $book = Book::create(['title' => $request->title, 'publication_year' => $request->publication_year]);
+        return redirect()->back();
+    }
+
 }
